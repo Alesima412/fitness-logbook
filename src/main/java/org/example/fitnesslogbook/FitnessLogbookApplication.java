66ce2923f243798ec5278039dbe4e7bf58691f72
Alesima412 <alexsidma@gmail.com>
2026-03-08 18:43:16 +0100

    Creation of the Project

---
+++ src/main/java/org/example/fitnesslogbook/FitnessLogbookApplication.java
@@ -0,0 +1,13 @@
+package org.example.fitnesslogbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessLogbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FitnessLogbookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/fitnesslogbook/FitnessLogbookApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.fitnesslogbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessLogbookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
